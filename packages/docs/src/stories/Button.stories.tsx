815ded8409532f1c197a367a9a89a8cff7abbcd5
douglasor <douglasor@outlook.com>
2023-02-24 17:56:10 -0300

    feat(UI):Added button to test doc with storybook

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,19 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Button, ButtonProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Button',
+  component: Button,
+  args: {
+    children: 'Enviar',
+  },
+} as Meta<ButtonProps>
+
+export const Default: StoryObj<typeof Button> = {}
+
+export const Larger: StoryObj<ButtonProps> = {
+  args: {
+    size: 'big',
+    children: 'BigButton',
+  },
+}
